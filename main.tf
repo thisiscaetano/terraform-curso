8c15433c8275f0c2ee46b76af6bf17fa2ac144c6
thisiscaetano <juncaetano@icloud.com>
2022-10-22 20:36:50 -0300

    usando o terraform import

--- main.tf
+++ main.tf
@@ -7,6 +7,52 @@ terraform {
 
 provider "aws" {
   profile                  = "default"
-  shared_credentials_files = "~/.aws/credentials"
+  shared_credentials_files = ["~/.aws/credentials"]
   region                   = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+    Environment : "dev"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "HelloWorld2"
+    Environment : "dev"
+  }
+}
+/*instancia importada da aws usando o terraform import*/
+resource "aws_instance" "web3" {
+  ami           = "ami-09d3b3274b6c5d4aa"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "import"
+    Environment : "dev"
+  }
 }
