726cdc5650aeb02f18d4572edd157c6e358056ce
thisiscaetano <juncaetano@icloud.com>
2022-10-22 17:35:59 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = ">= 4.0"
+  }
+}
+
+provider "aws" {
+  profile                  = "default"
+  shared_credentials_files = "~/.aws/credentials"
+  region                   = "us-east-1"
+}
