7260aff0728862f6db28fa110b35267bea2690bb
thisiscaetano <juncaetano@icloud.com>
2022-10-23 20:56:35 -0300

    usando count, looping na var name

--- main.tf
+++ main.tf
@@ -16,51 +16,16 @@ provider "aws" {
   region                   = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                     = data.aws_ami.ubuntu.id
-  instance_type           = var.int_type
-  disable_api_termination = var.disable_api_termination
-  tags = {
-    Name = var.int_name[0]
-    Environment : "dev"
-  }
-}
-
-resource "aws_instance" "web2" {
-  ami                     = data.aws_ami.ubuntu.id
-  instance_type           = var.int_type
-  disable_api_termination = var.disable_api_termination
-  
-  tags = {
-    Name = var.int_name[1]
-    Environment : "dev"
-  }
-}
-/*instancia importada da aws usando o terraform import*/
 resource "aws_instance" "web3" {
+  count                   = 3
   ami                     = var.amis[var.region]
   instance_type           = var.int_type
   disable_api_termination = var.disable_api_termination
-  user_data = file("./files/userdata.sh")
-  
+  user_data               = file("./files/userdata.sh")
+
   tags = {
-    Name = var.int_name[2]
+    Name = var.int_name[count.index]
     Environment : "dev"
   }
 }
